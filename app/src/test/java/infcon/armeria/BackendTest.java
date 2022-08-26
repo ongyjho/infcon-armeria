032fb1b8a259cb35b8fc0c8d737bbf3d6adb53ca
ongyjho <ongyjho@gmail.com>
2022-08-26 16:26:40 +0900

    .

--- app/src/test/java/infcon/armeria/BackendTest.java
+++ app/src/test/java/infcon/armeria/BackendTest.java
@@ -19,5 +19,9 @@ class BackendTest {
         final HttpResponse httpResponse = client.get("/foo");
         final CompletableFuture<AggregatedHttpResponse> future = httpResponse.aggregate();
         // 비동기 서버이므로 기다리지 않음. HttpResponse 껍데기만 갖고 있음. 한번에 안온다고 보는 게 편함
+
+        final AggregatedHttpResponse response = future.join();// blocking, 비동기서버에서는 사용하지 말것
+        System.err.println(response.contentUtf8());
     }
+
 }
