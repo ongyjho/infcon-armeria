3da691713c3624eab97c92847ba4d8a5bef80712
ongyjho <ongyjho@gmail.com>
2022-08-26 16:10:49 +0900

    .

--- app/src/main/java/infcon/armeria/Backend.java
+++ app/src/main/java/infcon/armeria/Backend.java
@@ -1,5 +1,23 @@
 package infcon.armeria;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+
 public final class Backend {
 
+    public static Backend of(String name, int port) {
+        return new Backend(name, port);
+    }
+
+    private final Server server;
+
+    private Backend(String name, int port) {
+        server = Server.builder().http(port).service("/foo", (ctx, req) -> {
+            return HttpResponse.of("Response from : " + name);
+        }).build();
+    }
+
+    public void start() {
+        server.start().join();
+    }
 }
