f77d68a2ee465831ed095065b51730b17c0384f7
ongyjho <ongyjho@gmail.com>
2022-08-26 16:23:31 +0900

    .

--- app/src/test/java/infcon/armeria/BackendTest.java
+++ app/src/test/java/infcon/armeria/BackendTest.java
@@ -1,11 +1,23 @@
 package infcon.armeria;
 
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
 
 class BackendTest {
 
     @Test
     void backend() {
+        final Backend foo = Backend.of("/foo", 8080);
+        foo.start();
 
+        final WebClient client = WebClient.of("http://localhost:8080");
+        System.err.println("Thread: " + Thread.currentThread().getName());
+        final HttpResponse httpResponse = client.get("/foo");
+        final CompletableFuture<AggregatedHttpResponse> future = httpResponse.aggregate();
+        // 비동기 서버이므로 기다리지 않음. HttpResponse 껍데기만 갖고 있음. 한번에 안온다고 보는 게 편함
     }
 }
