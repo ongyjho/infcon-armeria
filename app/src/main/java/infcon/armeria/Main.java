937be63578ae0630d2e89252b2f56d462177f0ec
ongyjho <ongyjho@gmail.com>
2022-08-26 16:03:41 +0900

    서버 생성

--- app/src/main/java/infcon/armeria/Main.java
+++ app/src/main/java/infcon/armeria/Main.java
@@ -1,8 +1,21 @@
 package infcon.armeria;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+
+import java.util.concurrent.CompletableFuture;
+
 public final class Main {
 
     public static void main(String[] args) {
+        final ServerBuilder serverBuilder = Server.builder();
 
+        final Server server = serverBuilder.http(8080).service("/infcon", (ctx, req) -> {
+            return HttpResponse.of("Hello, Armeria!");
+        }).build();
+
+        final CompletableFuture<Void> serverFuture = server.start();
+        serverFuture.join();
     }
 }
